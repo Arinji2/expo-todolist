25f38103f57e685f9b08158f127962d22d422cb6
Arinji2 <arinjaydhar205@gmail.com>
2025-08-31 10:45:20 +0530

    Add SafeAreaView

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,6 @@
-import { useState } from "react";
-import { Button, Text, View } from "react-native";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-	const [count, setCount] = useState(0);
-	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				backgroundColor: "black",
-				alignItems: "center",
-			}}
-		>
-			<Text className="text-white">
-				Edit ee app/index.tsx to edit this screen.
-			</Text>
-			<Button
-				onPress={() => setCount((count) => count + 1)}
-				title={`count is ${count}`}
-			/>
-		</View>
-	);
+	return <SafeAreaView className="w-full h-full bg-neutral-700"></SafeAreaView>;
 }
